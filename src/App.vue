eafd200d20f12dfb893b258286d634f452764d19
Michal Trubini <mchl.trubini@gmail.com>
2023-11-05 11:03:10 +0100

    various

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div class="max-w-[1440px] m-auto overflow-hidden">
     <TheHeader />
     <main>
-      <RouterView />
+      <router-view v-slot="slotProps">
+        <transition name="fade-button" mode="out-in">
+          <component :is="slotProps.Component"></component> </transition
+      ></router-view>
     </main>
     <TheFooter />
   </div>
@@ -11,10 +14,34 @@
 <script lang="ts">
 import TheHeader from "./components/header/TheHeader.vue";
 import TheFooter from "./components/footer/TheFooter.vue";
+import { ref, onBeforeMount, onBeforeUnmount, provide } from "vue";
+import { calculateIsSmallScreen } from "./functions/functions";
 export default {
   components: {
     TheHeader,
     TheFooter,
+  },
+  setup() {
+    const isSmallScreen = ref(calculateIsSmallScreen());
+
+    const handleResize = () => {
+      isSmallScreen.value = calculateIsSmallScreen();
+    };
+
+    onBeforeMount(() => {
+      window.addEventListener("resize", handleResize);
+      console.log(isSmallScreen.value)
+    });
+
+    onBeforeUnmount(() => {
+      window.removeEventListener("resize", handleResize);
+    });
+
+    provide("isSmallScreen", isSmallScreen);
+
+    return {
+      isSmallScreen,
+    };
   },
 };
 </script>
@@ -36,9 +63,31 @@ p {
   font-size: 0.9375rem;
 }
 
+.router-link-active {
+  color: #fcb72b;
+}
+
 @media screen and (min-width: 1440px) {
   body {
     text-align: left;
   }
+}
+
+.fade-button-enter-from,
+.fade-button-leave-to {
+  opacity: 0;
+}
+
+.fade-button-enter-active {
+  transition: opacity 0.1s ease-out;
+}
+
+.fade-button-leave-active {
+  transition: opacity 0.1s ease-in;
+}
+
+.fade-button-enter-to,
+.fade-button-leave-from {
+  opacity: 1;
 }
 </style>
